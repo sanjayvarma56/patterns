8bf147c3a5c9fc1d67c61d6ce90f666916c96c02
sanjayvarma56 <dssvarma56@gmail.com>
2026-06-02 09:57:16 +0530

    Initial commit

---
+++ patterns1.py
@@ -0,0 +1,102 @@
+#Square Star Pattern
+n= int(input("Enter the number: "))
+for i in range(0,n):
+    for j in range(0,n):
+        print("*",end=" ")
+    print()
+
+#Right Triangle    
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,i+1):
+#        print("*",end=" ")
+#    print()
+
+#Right Triangle
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,n):
+#        if (j<=i):
+#            print("*",end=" ")
+#    print()
+
+#Approach for Incremental Number Triangle
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,n):
+#        print(i,end=" ")
+#    print()
+
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,n):
+#        print(j,end=" ")
+#    print()
+    
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,n):
+#        print(j+1,end=" ")
+#    print()
+    
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,i):
+#        print(j+1,end=" ")
+#    print()
+
+#Incremental Number Triangle Pattern)
+#n=int(input("Enter the number: ")) 
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+#        print(j,end=" ")
+#    print()
+
+#Incremental Number Triangle
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,i+1):
+#        print(j+1,end=" ")
+#    print()
+
+#Reverse Incremental Number Triangle
+#n= int(input("Enter the number: "))
+#for i in range(n,0,-1):
+#    for j in range(0,i):
+#        print(j+1,end=" ")
+#    print()
+
+
+#(Repeated)Number Triangle Pattern
+#n=int(input("Enter the number: "))
+#for i in range(0,n):
+#    for j in range(0,i+1):
+#        print(i+1,end=" ")
+#    print()
+
+#(Repeated) Number Triangle Pattern
+#n=int(input("Enter the number: ")) 
+#for i in range(0,n):
+#    for j in range(0,i+1):
+#        if (j<=i):
+#            print(i+1,end=" ")
+#    print()
+
+#(Repeated) Number Triangle Pattern
+#n=int(input("Enter the number: ")) 
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+#        print(i,end=" ")
+#    print()
+
+#Reverse Right Triangle Pattern OR Inverted Star Triangle Pattern
+#n=int(input("Enter the number: "))
+#for i in range(n,0,-1):
+#    for j in range(0,i):
+#        print("*",end=" ")
+#    print()
+    
+#Reverse Right Triangle Pattern OR Inverted Star Triangle Pattern
+#n=int(input("Enter the number: "))
+#for i in range(n,0,-1):
+#    print("* " * i)
