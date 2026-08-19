411a0a3a234723c5114fb1a2dfe4ff262fa9f2f9
sanjayvarma56 <dssvarma56@gmail.com>
2026-08-19 09:58:06 +0530

    Pattern11(Reverse Pyramid)

---
+++ pattern11.py
@@ -0,0 +1,8 @@
+#To print a full reverse pyramid of stars
+n = int(input("Enter the number of rows: "))
+for i in range(n,0,-1):
+    for j in range(0,n-i):
+        print(end=" ")
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
